a551b263b76834ebecb131e344a2bf848194034c
Aviv Turgeman <avivturgeman10@gmail.com>
2024-03-12 15:39:46 +0200

    task-1

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const connect = require('./mongoConnection');
+require('dotenv').config();
+var cors = require("cors");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+
+connect()
+
+
+app.use('/', require('./routes'));
 
 app.listen(PORT ,() => {
     console.log(`Port is listing ${PORT}...`)
